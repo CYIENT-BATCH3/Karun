86420a33595e56326237cf2dd45c57f05ab43121
karun709 <111974164+karun709@users.noreply.github.com>
2022-08-26 20:46:09 +0530

    Create program_1.c

---
+++ WEEK_1/Day_1/program_1.c
@@ -0,0 +1,13 @@
+*WAP uses atleast 5 keywords in c and print some output using same?
+
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   char b;
+   float c;
+   double d;
+   printf("%d\n%d\n%d\n%d\n",sizeof(a),sizeof(b),sizeof(c),sizeof(d));   //printing the sizeof all the datatypes
+    return 0;
+}
