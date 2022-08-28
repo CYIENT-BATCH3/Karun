dc13a3821b97a58a7a06593901511770f2200406
karun709 <111974164+karun709@users.noreply.github.com>
2022-08-28 20:10:50 +0530

    Using relational and conditional operators

---
+++ WEEK_1/Day_5/program_4.c
@@ -0,0 +1,11 @@
+4.Use any 4 relational operators with conditional operators?
+
+#include <stdio.h>
+int main()
+{
+   int a=9,b=7;                         //declaring the variables a and b.
+   (a<=b)?puts("true"):puts("false");  // Using relational operator check the condition wheather it true or false  
+   (a==b)?puts("true"):puts("false");
+   (a!=b)?puts("true"):puts("false");
+   (a>=b)?puts("true"):puts("false");
+}
