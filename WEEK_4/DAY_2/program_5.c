9b1079b9130327fe2c76baefd0539a9505067c8f
karun709 <111974164+karun709@users.noreply.github.com>
2022-09-13 20:48:55 +0530

    Create program_5.c

---
+++ WEEK_4/DAY_2/program_5.c
@@ -0,0 +1,22 @@
+//WAP to write into a file using fprintf()
+#include<stdio.h>					//include standard input and output header file using preprocessor directive
+#include<stdlib.h>					//include the stdlib.h
+int main()							//start main()
+{
+	setbuf(stdout,NULL);
+	FILE *fp;						//declare a file pointer
+	char ch[100];						//declare a char ch to read and print the char with char
+	fp=fopen("C:\\Users\\au69911\\Desktop\\LeCo\\week4\\Day_1\\123.txt","w");
+	//open a file in read mode using fopen()
+	if(fp==NULL){					//if file not found print the below statement
+		printf("File not found\n");
+	}
+	else{
+		printf("File Found\n");
+	}
+	puts("enter the string");
+	scanf("%[^\n]s",ch);			//enter the string to write in a file
+	fprintf(fp,"%s",ch);			//write the string entered in the above step
+	fprintf(fp,"\n%s %s %s","I'm","in","office");		//another to write in to a file
+	fclose(fp);
+}
